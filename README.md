1d5d8bc6be0651eb92d7f8b37ca02b090e63bc60
Simon Fremaux <dievardump@gmail.com>
2021-06-22 12:10:52 +0200

    remove upgradeable from dep

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ The contract `./contracts/mocks/ERC721WithRoyalties.sol` shows how to extend ERC
 
 In this implementation, royalties are expected to be from 0 to 10000 which allows to define royalties with 2 decimals.
 
+# Installation
+
+Clone this repository using your favorite tool (i.e: `git clone git@github.com:dievardump/EIP2981-implementation.git`)
+
+In the directory of installation:
+
+`npm install`
+
+# Compilation
+
+`npm run compile`
+
+# Tests
+
+`npm run test`
--- contracts/ERC2981Royalties.sol
+++ contracts/ERC2981Royalties.sol
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/utils/introspection/ERC165Upgradeable.sol";
+import '@openzeppelin/contracts/utils/introspection/ERC165.sol';
 
-import "./IERC2981Royalties.sol";
+import './IERC2981Royalties.sol';
 
 /// @dev This is a contract used to add ERC2981 support to ERC721 and 1155
-///      This is a contract made to work with upgradeable contract,
-///      if you ever add state variables, you must update __gap accordingly.
-///      -> https://forum.openzeppelin.com/t/what-exactly-is-the-reason-for-uint256-50-private-gap/798
-///      if you do not use Upgradeable contracts, you should comment the line that defines __gap
-abstract contract ERC2981Royalties is ERC165Upgradeable, IERC2981Royalties {
+abstract contract ERC2981Royalties is ERC165, IERC2981Royalties {
     struct Royalty {
         address recipient;
         uint256 value;
@@ -18,11 +14,7 @@ abstract contract ERC2981Royalties is ERC165Upgradeable, IERC2981Royalties {
 
     mapping(uint256 => Royalty) internal _royalties;
 
-    /// @dev If you ever add new state variable please add BEFORE this comment, and update __gap accordingly
-    /// @dev https://forum.openzeppelin.com/t/what-exactly-is-the-reason-for-uint256-50-private-gap/798
-    uint256[10] private __gap;
-
-    /// @inheritdoc	ERC165Upgradeable
+    /// @inheritdoc	ERC165
     function supportsInterface(bytes4 interfaceId)
         public
         view
@@ -42,7 +34,7 @@ abstract contract ERC2981Royalties is ERC165Upgradeable, IERC2981Royalties {
         address recipient,
         uint256 value
     ) internal {
-        require(value <= 10000, "ERC2981Royalties: Too high");
+        require(value <= 10000, 'ERC2981Royalties: Too high');
 
         _royalties[id] = Royalty(recipient, value);
     }
