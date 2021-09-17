8aa44e173c0bddc3039946d9d9bf6b32de551e41
Simon Fremaux <simon.fremaux@gmail.com>
2021-09-17 12:09:01 +0200

    fix support ERC165 interface

--- contracts/ERC2981PerTokenRoyalties.sol
+++ contracts/ERC2981PerTokenRoyalties.sol
@@ -22,7 +22,7 @@ abstract contract ERC2981PerTokenRoyalties is ERC165, IERC2981Royalties {
         override
         returns (bool)
     {
-        return interfaceId == type(IERC2981Royalties).interfaceId;
+        return interfaceId == type(IERC2981Royalties).interfaceId || super.supportsInterface(interfaceId);
     }
 
     /// @dev Sets token royalties
