63b95847d2e4f56ebd8fe5da4b250cb1ab1978ab
Blaine Malone <blainemalone01@gmail.com>
2021-10-07 12:07:38 -0400

    fix: remove commented code.

--- contracts/ERC2981Base.sol
+++ contracts/ERC2981Base.sol
@@ -19,26 +19,4 @@ abstract contract ERC2981Base is ERC165, IERC2981Royalties {
             interfaceId == type(IERC2981Royalties).interfaceId ||
             super.supportsInterface(interfaceId);
     }
-
-    // /// @notice Uses Bitpacking to encode royalties into one bytes32 (saves gas)
-    // /// @return the bytes32 representation
-    // function encodeRoyalties(address recipient, uint256 amount)
-    //     public
-    //     pure
-    //     returns (bytes32)
-    // {
-    //     require(amount <= 10000, '!WRONG_AMOUNT!');
-    //     return bytes32((uint256(uint160(recipient)) << 96) + amount);
-    // }
-
-    // /// @notice Uses Bitpacking to decode royalties from a bytes32
-    // /// @return recipient and amount
-    // function decodeRoyalties(bytes32 royalties)
-    //     public
-    //     pure
-    //     returns (address recipient, uint256 amount)
-    // {
-    //     recipient = address(uint160(uint256(royalties) >> 96));
-    //     amount = uint256(uint96(uint256(royalties)));
-    // }
 }
